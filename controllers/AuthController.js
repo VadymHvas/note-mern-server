ab920045038f59215088e596600e469d99d5f9b2
novakoriss <121388611+VadymHvas@users.noreply.github.com>
2023-07-03 22:30:47 +0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,7 +4,6 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv/config.js";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import fs from "fs";
 
 export const registerUser = async (req, res) => {
     const { username, password } = req.body;
